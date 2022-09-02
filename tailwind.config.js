ec098c082a0591975a2cfac250003798a2b16721
usama508 <musama023418@gmail.com>
2022-09-02 04:45:48 -0700

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [  
-    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")
-],
+  plugins: [],
 }
