370c23626c0483176ffb137f10f24d753643365e
usama508 <musama023418@gmail.com>
2022-09-05 03:58:02 -0700

    new commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Swipper from "../Components/Swipper";
 
 
 
-export default function Home() {
+export default function Home({data}) {
 
   return (
     <div className="z-0">
@@ -33,7 +33,7 @@ export default function Home() {
       </div>
 
       <div className="m-4">
-        <Table />
+        <Table data={data} />
       </div>
       <div>
         <ScrollToTop/>
@@ -41,4 +41,16 @@ export default function Home() {
      
     </div>
   );
+}
+
+export async function getServerSideProps(){
+  const response = await fetch(
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  const data = await response.json()
+  return{
+    props:{
+      data
+    }
+  }
+
 }
